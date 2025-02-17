4186066760c6f4e5db4503f9528832e67163b2c0
Abdurazakm <abdulkem5472@gmail.com>
2025-02-17 09:13:22 +0300

    codeforece

---
+++ F_Codeforces_Checking.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    ch = input()
+    if ch == 'c' or ch == 'o' or ch == 'd' or ch == 'e' or ch == 'f' or ch == 'r' or ch == 's':
+        print("YES")
+    else:
+        print("NO")
