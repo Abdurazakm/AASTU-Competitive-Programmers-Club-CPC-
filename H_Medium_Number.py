a17f10b21302faaaa577263fea31a3ca1e07a2d3
Abdurazakm <abdulkem5472@gmail.com>
2025-02-17 09:13:50 +0300

    medium

---
+++ H_Medium_Number.py
@@ -0,0 +1,9 @@
+t = int(input())
+for _ in range(t):
+    a,b,c = map(int, input().split())
+    if a < b < c or c < b < a:
+        print(b)
+    elif b < a < c or c < a < b:
+        print(a)
+    elif b < c < a or a < c < b:
+        print(c)
